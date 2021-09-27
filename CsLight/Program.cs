0354ff6f17200d3816d3b769955fb8dc1ca48a39
Putseyeu <putseyeu.a.d@gmail.com>
2021-09-27 22:33:00 +0200

    test
    test

---
+++ CsLight/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameShip = "merry drowned man";
+            int totalSailors = 90;
+            float totalGunpowder = 1234f;
+            bool afloat = true;
+            byte totalCapitan = 1;
+            ushort totalKernels = 202;
+            uint totalCannons = 50;
+            long milesGoal = 350;
+            ulong minWay = 2400;
+            object literRom = 299.5;  
+        }
+    }
+}
